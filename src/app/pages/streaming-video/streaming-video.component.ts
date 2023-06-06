5126d4d861342c09705169209f1887c88f757fa6
Junhao Chen <junhao@hotmail.com>
2023-06-06 13:37:11 +0200

    Added bootstrap working

---
+++ src/app/pages/streaming-video/streaming-video.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-streaming-video',
+    templateUrl: './streaming-video.component.html',
+    styleUrls: ['./streaming-video.component.css'],
+})
+export class StreamingVideoComponent {}
