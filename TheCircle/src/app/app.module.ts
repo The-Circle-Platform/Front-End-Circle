748249585475b5a7b29037ca3b9c0b99fd84f1ef
Thomas <vanotterloo.thomas@gmail.com>
2023-06-06 12:02:40 +0200

    Added folder construction

--- TheCircle/src/app/app.module.ts
+++ TheCircle/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminPageComponent } from './pages/admin/admin-page/admin-page.component';
+import { BroadcastPageComponent } from './pages/broadcast/broadcast-page/broadcast-page.component';
+import { HomePageComponent } from './pages/homepage/home-page/home-page.component';
+import { ProfilePageComponent } from './pages/profile/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPageComponent,
+    BroadcastPageComponent,
+    HomePageComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule
