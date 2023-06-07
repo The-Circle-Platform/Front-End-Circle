be45d01dd2983f6f71832de4dcb1e40882c46825
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-06-07 11:09:25 +0200

    login pagina gemaakt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/homepage/home-page/home-page.component';
-import {OnlineListComponent} from "./pages/homepage/online-list/online-list.component";
-import {ProfilePageComponent} from "./pages/profile/profile-page/profile-page.component";
-import {BroadcastPageComponent} from "./pages/broadcast/broadcast-page/broadcast-page.component";
-import {StreamPageComponent} from "./pages/broadcast/stream-page/stream-page.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePageComponent } from "./pages/homepage/home-page/home-page.component";
+import { OnlineListComponent } from "./pages/homepage/online-list/online-list.component";
+import { ProfilePageComponent } from "./pages/profile/profile-page/profile-page.component";
+import { BroadcastPageComponent } from "./pages/broadcast/broadcast-page/broadcast-page.component";
+import { StreamPageComponent } from "./pages/broadcast/stream-page/stream-page.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'homepage' },
-    { path: 'homepage', pathMatch: 'full', component: HomePageComponent },
-    { path: 'listStreamers', pathMatch: 'full', component:OnlineListComponent},
-    { path: 'Profile', pathMatch: 'full', component: ProfilePageComponent},
-    { path: 'Stream', pathMatch: 'full', component: StreamPageComponent},
-    { path: '**', redirectTo: 'homepage' },
-
+  { path: "", pathMatch: "full", redirectTo: "homepage" },
+  { path: "homepage", pathMatch: "full", component: HomePageComponent },
+  { path: "listStreamers", pathMatch: "full", component: OnlineListComponent },
+  { path: "Profile", pathMatch: "full", component: ProfilePageComponent },
+  { path: "Stream", pathMatch: "full", component: StreamPageComponent },
+  { path: "Login", pathMatch: "full", component: LoginComponent },
+  { path: "**", redirectTo: "homepage" },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
