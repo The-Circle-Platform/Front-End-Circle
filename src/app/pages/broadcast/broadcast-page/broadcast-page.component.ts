f564391c253a78abc838dd630111e29177453e41
Matthijs <matthijs07g@gmail.com>
2023-06-07 13:00:47 +0200

    stream variabele bepaalt

--- src/app/pages/broadcast/broadcast-page/broadcast-page.component.ts
+++ src/app/pages/broadcast/broadcast-page/broadcast-page.component.ts
@@ -1,46 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-broadcast-page',
-  templateUrl: './broadcast-page.component.html',
-  styleUrls: ['./broadcast-page.component.css']
+  selector: "app-broadcast-page",
+  templateUrl: "./broadcast-page.component.html",
+  styleUrls: ["./broadcast-page.component.css"],
 })
-
-export class BroadcastPageComponent implements OnInit{
-  
+export class BroadcastPageComponent implements OnInit {
   constructor() {}
 
-  videoref:any;
-   ngOnInit(): void {
-       this.videoref = document.getElementById('webcam');
-       console.log(this.videoref);
-       this.setupCamera();
-   }
+  videoref: any;
+  ngOnInit(): void {
+    this.videoref = document.getElementById("webcam");
+    console.log(this.videoref);
+    this.setupCamera();
+  }
 
-   setupCamera(){
-      navigator.mediaDevices.getUserMedia({
-        video:{width:800, height:550},
-        audio:true
-      }).then(stram =>{
+  setupCamera() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 800, height: 550 },
+        audio: true,
+      })
+      .then((stram) => {
         console.log(stram);
         this.videoref.srcObject = stram;
-      })
-   }
+      });
+  }
 
-   cameraOn(){
-    
+  cameraOn() {
     this.ngOnInit();
-    console.log('1')
-   }
-   cameraOff(){
-    this.videoref.srcObject= null;
-   }
+  }
 
-   startStream(){
+  cameraOff() {
+    this.videoref.srcObject = null;
+  }
 
-   }
+  startStream() {
+    console.log(this.videoref.srcObject);
+  }
 
-   endStream(){
-
-   }
+  endStream() {}
 }
