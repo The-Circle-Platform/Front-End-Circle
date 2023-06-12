079deb01676424bb469d507ee691ab894a48eef0
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-06-12 09:39:46 +0200

    replace email with username

--- src/app/services/authServices/auth.service.ts
+++ src/app/services/authServices/auth.service.ts
@@ -1,150 +1,136 @@
-import jwt_decode from 'jwt-decode';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { Router } from '@angular/router';
-import { map, catchError, switchMap } from 'rxjs/operators';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ConfigService } from 'src/app/shared/moduleconfig/config.service';
+import jwt_decode from "jwt-decode";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { Router } from "@angular/router";
+import { map, catchError, switchMap } from "rxjs/operators";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { ConfigService } from "src/app/shared/moduleconfig/config.service";
 import {
-    IToken,
-    IRegister,
-    UserIdentity,
-    UserInfo,
-    ILogin,
-} from '../../Domain/Models/User';
+  IToken,
+  IRegister,
+  UserIdentity,
+  UserInfo,
+  ILogin,
+} from "../../Domain/Models/User";
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: "root",
 })
 export class AuthService {
-    public currentUser$ = new BehaviorSubject<IToken | undefined>(undefined);
-    private readonly CURRENT_USER = 'currentuser';
-    private readonly headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-    });
-    private siteEndpoint: string;
+  public currentUser$ = new BehaviorSubject<IToken | undefined>(undefined);
+  private readonly CURRENT_USER = "currentuser";
+  private readonly headers = new HttpHeaders({
+    "Content-Type": "application/json",
+  });
+  private siteEndpoint: string;
 
-    constructor(
-        private configService: ConfigService,
-        private http: HttpClient,
-        private router: Router
-    ) {
-        this.siteEndpoint = `${this.configService.getConfig().apiEndpoint}api`;
+  constructor(
+    private configService: ConfigService,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.siteEndpoint = `${this.configService.getConfig().apiEndpoint}api/auth`;
 
-        this.getUserFromLocalStorage()
-            .pipe(
-                switchMap((user: IToken | undefined) => {
-                    if (user) {
-                        this.currentUser$.next(user);
-                        console.log(
-                            'User from local storage:',
-                            this.decodeJwtToken(
-                                this.getAuthorizationToken() || ''
-                            )
-                        );
-                        return of(user);
-                    } else {
-                        return of(undefined);
-                    }
-                })
-            )
-            .subscribe();
-    }
-
-    decodeJwtToken(token: string) {
-        return token ? jwt_decode(token) : null;
-    }
-
-    login(
-        emailAddress: string,
-        password: string
-    ): Observable<ILogin | undefined> {
-        const credentials = {
-            emailAddress: emailAddress,
-            password: password,
-        };
-
-        return this.http
-            .post<UserIdentity>(`${this.siteEndpoint}/login`, credentials, {
-                headers: this.headers,
-            })
-            .pipe(
-                map((data: any) => {
-                    location.reload();
-                    localStorage.setItem(
-                        this.CURRENT_USER,
-                        JSON.stringify(data.results)
-                    );
-                    this.currentUser$.next(data);
-                    return data;
-                }),
-                catchError((error) => {
-                    console.log('Error message:', error.error.message);
-                    return of(undefined);
-                })
+    this.getUserFromLocalStorage()
+      .pipe(
+        switchMap((user: IToken | undefined) => {
+          if (user) {
+            this.currentUser$.next(user);
+            console.log(
+              "User from local storage:",
+              this.decodeJwtToken(this.getAuthorizationToken() || "")
             );
-    }
-
-    register(userData: IRegister): Observable<UserInfo | undefined> {
-        return this.http
-            .post<UserInfo>(`${this.siteEndpoint}/register`, userData, {
-                headers: this.headers,
-            })
-            .pipe(
-                map((data: any) => {
-                    localStorage.setItem(
-                        this.CURRENT_USER,
-                        JSON.stringify(data.results)
-                    );
-                    this.currentUser$.next(data);
-                    return data;
-                }),
-                catchError((error) => {
-                    console.log('Error message:', error.error.message);
-                    return of(undefined);
-                })
-            );
-    }
-
-    logout(): void {
-        this.router
-            .navigate(['/'])
-            .then((success) => {
-                if (success) {
-                    console.log('logout - removing local user info');
-                    localStorage.removeItem(this.CURRENT_USER);
-                    this.currentUser$.next(undefined);
-                } else {
-                    console.log('navigate result:', success);
-                }
-            })
-            .catch((error) =>
-                console.log('Error message:', error.error.message)
-            );
-    }
-
-    getUserFromLocalStorage(): Observable<UserInfo | undefined> {
-        const user = localStorage.getItem(this.CURRENT_USER);
-
-        if (user) {
-            const localUser = JSON.parse(user);
-            return of(localUser);
-        } else {
+            return of(user);
+          } else {
             return of(undefined);
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  decodeJwtToken(token: string) {
+    return token ? jwt_decode(token) : null;
+  }
+
+  login(
+    emailAddress: string,
+    password: string
+  ): Observable<ILogin | undefined> {
+    const credentials = {
+      emailAddress: emailAddress,
+      password: password,
+    };
+
+    return this.http
+      .post<UserIdentity>(`${this.siteEndpoint}/login`, credentials, {
+        headers: this.headers,
+      })
+      .pipe(
+        map((data: any) => {
+          location.reload();
+          localStorage.setItem(this.CURRENT_USER, JSON.stringify(data.results));
+          this.currentUser$.next(data);
+          return data;
+        }),
+        catchError((error) => {
+          console.log("Error message:", error.error.message);
+          return of(undefined);
+        })
+      );
+  }
+
+  register(userData: IRegister): Observable<UserInfo | undefined> {
+    return this.http
+      .post<UserInfo>(`${this.siteEndpoint}/register`, userData, {
+        headers: this.headers,
+      })
+      .pipe(
+        map((data: any) => {
+          localStorage.setItem(this.CURRENT_USER, JSON.stringify(data.results));
+          this.currentUser$.next(data);
+          return data;
+        }),
+        catchError((error) => {
+          console.log("Error message:", error.error.message);
+          return of(undefined);
+        })
+      );
+  }
+
+  logout(): void {
+    this.router
+      .navigate(["/"])
+      .then((success) => {
+        if (success) {
+          console.log("logout - removing local user info");
+          localStorage.removeItem(this.CURRENT_USER);
+          this.currentUser$.next(undefined);
+        } else {
+          console.log("navigate result:", success);
         }
-    }
+      })
+      .catch((error) => console.log("Error message:", error.error.message));
+  }
 
-    getAuthorizationToken(): string | undefined {
-        const user = JSON.parse(
-            localStorage.getItem(this.CURRENT_USER) || '{}'
-        );
-        return user.token;
-    }
+  getUserFromLocalStorage(): Observable<UserInfo | undefined> {
+    const user = localStorage.getItem(this.CURRENT_USER);
 
-    getCurrentUserId(): string | undefined {
-        const user = JSON.parse(
-            localStorage.getItem(this.CURRENT_USER) || '{}'
-        );
-        return user.id;
+    if (user) {
+      const localUser = JSON.parse(user);
+      return of(localUser);
+    } else {
+      return of(undefined);
     }
+  }
+
+  getAuthorizationToken(): string | undefined {
+    const user = JSON.parse(localStorage.getItem(this.CURRENT_USER) || "{}");
+    return user.token;
+  }
+
+  getCurrentUserId(): string | undefined {
+    const user = JSON.parse(localStorage.getItem(this.CURRENT_USER) || "{}");
+    return user.id;
+  }
 }
