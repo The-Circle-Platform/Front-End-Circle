730ccc75ff194159a98173578d367e6b92f3853b
Thomas <vanotterloo.thomas@gmail.com>
2023-06-20 16:50:56 +0200

    Deleted 2 imports

--- src/app/services/vidStream/VidStream.service.ts
+++ src/app/services/vidStream/VidStream.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { VideoStream } from "src/app/Domain/Models/Stream";
-import { ConfigModule } from "src/app/shared/moduleconfig/config.module";
 import { ConfigService } from "src/app/shared/moduleconfig/config.service";
 
 @Injectable({providedIn: 'root'})
